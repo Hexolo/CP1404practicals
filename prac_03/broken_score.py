362cb59fe19654cea5afc926cfe9d17dd56ceb04
Hexolo <thannigel@gmail.com>
2022-03-31 13:49:54 +0800

    Refactor score program into function

--- prac_03/broken_score.py
+++ prac_03/broken_score.py
@@ -1,10 +1,18 @@
-score = float(input("Enter score: "))
-while score < 0 or score > 100:
-    print("Invalid score")
+def main():
     score = float(input("Enter score: "))
-if 90 <= score <= 100:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+    while score < 0 or score > 100:
+        print("Invalid score")
+        score = float(input("Enter score: "))
+    print(determine_grade(score))
+
+
+def determine_grade(score):
+    if 90 <= score <= 100:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+
+main()
