141b4b2c4509cc76eb3ae29e8b23f5855ce04a7b
Hexolo <thannigel@gmail.com>
2022-03-31 13:14:06 +0800

    Add password check program

---
+++ prac_03/password_checker.py
@@ -0,0 +1,21 @@
+MIN_LENGTH = 6
+MAX_LENGTH = 15
+
+
+def main():
+    """Program to get and check user's password"""
+    password = input("Please enter you new password: ")
+    while not is_valid_password(password):
+        print(f"Invalid password: Your password must be between {MIN_LENGTH} and {MAX_LENGTH} characters")
+        password = input("Please enter you new password: ")
+    for char in password:
+        print("*", end=" ")
+
+
+def is_valid_password(password):
+    if len(password) < MIN_LENGTH or len(password) > MAX_LENGTH:
+        return False
+    return True
+
+
+main()
