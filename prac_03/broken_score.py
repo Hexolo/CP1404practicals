22c68e2bf139c844ed77a98cc3d08d954fb280d2
Hexolo <thannigel@gmail.com>
2022-03-31 13:55:58 +0800

    Add program in main function that generate random score

--- prac_03/broken_score.py
+++ prac_03/broken_score.py
@@ -1,9 +1,13 @@
+import random
+
 def main():
     score = float(input("Enter score: "))
     while score < 0 or score > 100:
         print("Invalid score")
         score = float(input("Enter score: "))
     print(determine_grade(score))
+    random_score = random.randint(0, 100)
+    print(f"Random score: {random_score}\t", determine_grade(random_score))
 
 
 def determine_grade(score):
