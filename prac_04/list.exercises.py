6a8655b091a3e239afad65faec039193ace9d10e
Hexolo <thannigel@gmail.com>
2022-04-06 19:28:38 +0800

    add a program that output various result from a list

---
+++ prac_04/list.exercises.py
@@ -0,0 +1,9 @@
+numbers = []
+for i in range(1, 6):
+    number = int(input("Number: "))
+    numbers.append(number)
+print(f"The first number is {numbers[0]}")
+print(f"The last number is {numbers[-1]}")
+print(f"The smallest number is {min(numbers)}")
+print(f"The largest number is {max(numbers)}")
+print(f"The average of the numbers is {sum(numbers)/len(numbers)}")
