51dabe3eb66614e6f489b1a3bcaa52d779725348
Hexolo <thannigel@gmail.com>
2022-04-06 18:18:45 +0800

    fix the program format

--- prac_04/total_income.py
+++ prac_04/total_income.py
@@ -7,15 +7,15 @@ Starter code for cumulative total income program
 def main():
     """Display income report for incomes over a given number of months."""
     incomes = []
-    months = int(input("How many months? "))
+    num_of_month = int(input("How many months? "))
 
-    for month in range(1, months + 1):
-        income = float(input("Enter income for month " + str(month) + ": "))
+    for month in range(1, num_of_month + 1):
+        income = float(input(f"Enter income for month {month}: "))
         incomes.append(income)
 
     print("\nIncome Report\n-------------")
     total = 0
-    for month in range(1, months + 1):
+    for month in range(1, num_of_month + 1):
         income = incomes[month - 1]
         total += income
         print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
