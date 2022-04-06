e6fe318548bebf1cfba57bc59f3f273a93be572c
Hexolo <thannigel@gmail.com>
2022-04-06 19:33:30 +0800

    add a program that ask for authorised username

--- prac_04/list.exercises.py
+++ prac_04/list.exercises.py
@@ -7,3 +7,10 @@ print(f"The last number is {numbers[-1]}")
 print(f"The smallest number is {min(numbers)}")
 print(f"The largest number is {max(numbers)}")
 print(f"The average of the numbers is {sum(numbers)/len(numbers)}")
+
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+username = input("Username: ")
+while username not in usernames:
+    print("Access Denied")
+    username = input("Username: ")
+print("Access Granted")
