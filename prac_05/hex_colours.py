91fcdc7ff8d722f4c347aebe456d9d097ef6a4ec
Hexolo <thannigel@gmail.com>
2022-04-13 16:27:17 +0800

    add loop that ask for user colour

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -4,3 +4,10 @@ NAME_TO_CODE = {"Absolute Zero": "#0048ba",
                 "Baby Pink": "#f4c2c2", "Bitter Lemon": "#cae00d", "Bitter Lime": "#bfff00",
                 "Black": "#000000"}
 
+colour = input("Enter colour name: ").title()
+while colour != "":
+    if colour in NAME_TO_CODE:
+        print(colour, "is", NAME_TO_CODE[colour])
+    else:
+        print("Invalid colour")
+    colour = input("Enter colour name: ").title()
