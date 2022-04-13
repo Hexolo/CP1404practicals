d058c87c7cda2e890f7a1a0e5491cae960af53ae
Hexolo <thannigel@gmail.com>
2022-04-13 16:26:29 +0800

    add dictionary for hex_colours.py

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,6 @@
+NAME_TO_CODE = {"Absolute Zero": "#0048ba",
+                "Acid Green": "#b0bf1a", "Alizarin Crimson": "#e32636",
+                "Amber": "#ffbf00", "Aqua": "#00ffff", "BabyBlue": "#89cff0",
+                "Baby Pink": "#f4c2c2", "Bitter Lemon": "#cae00d", "Bitter Lime": "#bfff00",
+                "Black": "#000000"}
+
