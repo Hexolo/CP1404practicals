613aba3d73f8bcfb54bf8fc08a02133a808486ad
Hexolo <thannigel@gmail.com>
2022-05-01 22:12:13 +0800

    Add init constructor

---
+++ prac_06/programming_languange.py
@@ -0,0 +1,9 @@
+
+class ProgrammingLanguage:
+    def __init__(self, field="", typing="", reflection="", year=0):
+        self.field = field
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+   
