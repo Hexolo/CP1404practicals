253e9bd2a0f557d9d96249610cd64868f9843969
Hexolo <thannigel@gmail.com>
2022-05-01 23:02:03 +0800

    Add get_age and is_vintage method in guitar.py

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -2,4 +2,16 @@ class Guitar:
     def __init__(self, name="", year=0, cost=0):
         self.name = name
         self.year = year
-        self. cost = cost
+        self.cost = cost
+
+    def __str__(self):
+        return "{}({}): ${}".format(self.name, self.year, self.cost)
+
+    def get_age(self):
+        return 2022 - self.year
+
+    def is_vintage(self):
+        if self.get_age() > 50:
+            return True
+        else:
+            return False
