725f206ffe3decd8e1b4565d6f754e9c986a18e6
Hexolo <thannigel@gmail.com>
2022-05-01 22:15:12 +0800

    Add languages.py

---
+++ prac_06/languages.py
@@ -0,0 +1,5 @@
+from prac_06.programming_languange import ProgrammingLanguage
+
+ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
