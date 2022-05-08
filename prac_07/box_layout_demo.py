18c7529478385e944f6cf3f68e1bb03e02d19791
Than Tan Dung <77910025+Hexolo@users.noreply.github.com>
2022-05-09 05:40:33 +0800

    Modify boxlayout.kv/py

---
+++ prac_07/box_layout_demo.py
@@ -0,0 +1,21 @@
+from kivy.app import App
+from kivy.lang import Builder
+
+
+class BoxLayoutDemo(App):
+    def build(self):
+        self.title = "Box Layout Demo"
+        self.root = Builder.load_file('box_layout.kv')
+        return self.root
+
+    def handle_greet(self):
+        print('greet')
+        print("test")
+        self.root.ids.output_label.text = "Hello" + self.root.ids.input_name.text
+
+    def handle_clear(self):
+        self.root.ids.output_label.text = "Hello"
+        self.root.ids.input_name.text = ''
+
+
+BoxLayoutDemo().run()
