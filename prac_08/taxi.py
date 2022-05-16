341e667e2bcf254464befe792218c3a6dbacdae1
Hexolo <thannigel@gmail.com>
2022-05-16 20:44:47 +0800

    Testing for taxi class

--- prac_08/taxi.py
+++ prac_08/taxi.py
@@ -7,11 +7,11 @@ from prac_08.car import Car
 
 class Taxi(Car):
     """Specialised version of a Car that includes fare costs."""
+    price_per_km = 1.23
 
-    def __init__(self, name, fuel, price_per_km):
+    def __init__(self, name, fuel):
         """Initialise a Taxi instance, based on parent class Car."""
         super().__init__(name, fuel)
-        self.price_per_km = price_per_km
         self.current_fare_distance = 0
 
     def __str__(self):
